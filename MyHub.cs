6bc9a8ff2658938e322fd659d234bad1985b22d2
Hergu <Hergu@DESKTOP-L35LFQ7>
2022-12-21 22:25:12 +0300

    Proje dosyası ekle.

---
+++ MyHub.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class MyHub : Hub
+{
+    private const string API_URL = "wss://stream.bybit.com/realtime";
+
+    private ClientWebSocket _ws;
+    private CancellationTokenSource _cts;
+
+    public async Task Connect()
+    {
+        _ws = new ClientWebSocket();
+        _cts = new CancellationTokenSource();
+
+        await _ws.ConnectAsync(new Uri(API_URL), _cts.Token);
+
+        var subscriptionMessage = "{\"op\": \"subscribe\", \"args\": [\"trade.BTCUSD\"]}";
+        await _ws.SendAsync(Encoding.UTF8.GetBytes(subscriptionMessage), WebSocketMessageType.Text, true, _cts.Token);
+
+        await Task.Factory.StartNew(
+            async () =>
+            {
+                var buffer = new byte[1024 * 4];
+
+                while (_ws.State == WebSocketState.Open)
+                {
+                    var result = await _ws.ReceiveAsync(new ArraySegment<byte>(buffer), _cts.Token);
+
+                    Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, result.Count));
+                    
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await _ws.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, _cts.Token);
+                        _cts.Cancel();
+                    }
+                    else
+                    {
+                        var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                        var bybitMessage = JsonConvert.DeserializeObject<ByBitMessage>(message);
+                        if (bybitMessage.Type == "data")
+                        {
+                            await Clients.All.SendAsync("ReceiveMessage", bybitMessage.Data);
+
+                        }
+                    }
+                }
+            }, _cts.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+    }
+
+    public async Task Disconnect()
+    {
+        _cts.Cancel();
+        await _ws.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, _cts.Token);
+        _ws.Dispose();
+    }
+}
+
+public class ByBitMessage
+{
+    [JsonProperty("type")]
+    public string Type { get; set; }
+
+    [JsonProperty("data")]
+    public object Data { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Text;
+using System.Net.WebSockets;
+using System;
+using Microsoft.AspNetCore.SignalR;
+
+MyHub kk = new MyHub();
+await kk.Connect();
+Console.WriteLine("finish");
+//string a = "";
+//byte[] b = new byte[256];
+//var ws = new ClientWebSocket();
+//await ws.ConnectAsync(new Uri("wss://nbstream.binance.com/eoptions/stream?streams=BTC-210630-9000-P@trade/BTC-210630-9000-P@ticker"),CancellationToken.None);
+//Console.WriteLine(ws.State);
+
+//var result = await ws.ReceiveAsync(b, CancellationToken.None);
+//Console.WriteLine(ws.State);
+//Console.WriteLine(result.Count);
+//Console.WriteLine(ws.CloseStatusDescription);
+//Console.WriteLine(ws.CloseStatus);
+//Console.WriteLine(Encoding.ASCII.GetString(b, 0, result.Count));
+
+
+
+//using (var ws = new ClientWebSocket())
+//{
+//    Console.WriteLine("using içinde");
+//    await ws.ConnectAsync(new Uri("wss://nbstream.binance.com/eoptions/ws/BTC-210630-9000-P@ticker"), CancellationToken.None);
+//    var buffer = new byte[256];
+//    Console.WriteLine("while girecek");
+//    while (ws.State == WebSocketState.Open)
+//    {
+//        Console.WriteLine("while girdi");
+//        var result = await ws.ReceiveAsync(buffer, CancellationToken.None);
+//        Console.WriteLine("ife girecek");
+        
+//        if (result.MessageType == WebSocketMessageType.Close)
+//        {
+//            Console.WriteLine("if girdi");
+//            await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+//        }
+//        else
+//        {
+//            Console.WriteLine("elseye girdi");
+//            Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, result.Count));
+//        }
+//    }
+//    Console.WriteLine("while dan çıktı");
+//}
+//Console.WriteLine("usingden çıktı");
